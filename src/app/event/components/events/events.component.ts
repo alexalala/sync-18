76708325dc8c9224ceea71d34aa25fc3de746452
Alexandra Moore <alexmoore94@hotmail.co.uk>
2018-11-16 19:51:09 +0000

    events page styled responsively and logo added in for header

--- src/app/event/components/events/events.component.ts
+++ src/app/event/components/events/events.component.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.css']
+  styleUrls: ['./events.component.scss']
 })
 export class EventsComponent implements OnInit {
 
